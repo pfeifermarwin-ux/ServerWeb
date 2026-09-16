7422bef75f435191cfc37bedb5531517f7e31840
pfeifermarwin-ux <pfeifermarwin@gmail.com>
2026-09-16 16:07:02 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const loginStatus = localStorage.getItem('loginStatus');
 
 if (loginStatus === 'true') {
-    alert('Login')
+    window.location.href = '/dashboard'
 }else {
     window.location.href = '/login';
 }
