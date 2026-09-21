2f5d72dcfefcca0529b15b2c66bf096e98983b10
pfeifermarwin-ux <pfeifermarwin@gmail.com>
2026-09-21 15:53:38 +0200

    Update dashboard.js

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -27,6 +27,7 @@ async function checkTokenValidity() {
         body: JSON.stringify({ username: username, token: token}),
     });
     const data = await response.json();
+    console.log(response)
     if (response.status){
         localStorage.setItem('loginStatus', 'true');
     }else {
