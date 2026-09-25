7045f6ab8e168aea2f69f287fee7f24f0f18dee3
pfeifermarwin-ux <pfeifermarwin@gmail.com>
2026-09-25 15:28:39 +0200

    Update dashboard.js

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -158,7 +158,7 @@ siteBarManageUserBTN.onclick = async () => {
         newRow.insertCell(3).textContent = user.role;
 
         newRow.onclick = () => {
-            await.openNotification("Table click", `Username: ${user.username}`)
+            console.log(user.username)
         };
     });
 };
